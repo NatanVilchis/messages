0cd1393690c366af60dc967d56f56d297daac2ae
Natan Vilchis <contacto@natanvilchis.org>
2025-06-16 10:03:01 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,6 +4,9 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 import base64 
+import os
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
 
 kdf = PBKDF2HMAC(
     algorithm=hashes.SHA512(),
@@ -16,3 +19,15 @@ key = hashlib.sha512("WORLD".encode()).digest()
 key = base64.urlsafe_b64encode(key)
 print(key)
 f = Fernet(key)
+
+# f.decrypt
+
+#key = hashlib.sha256("HELLO".encode()).digest()
+#aesgcm = AESGCM(key)
+#aad = "WORLD".encode()
+#nonce = "HELLO".encode()
+#ct = aesgcm.encrypt(nonce, message,aad)
+#print(ct)
+
+#plaintext = aesgcm.decrypt(nonce, ct, aad)
+#print(plaintext)
