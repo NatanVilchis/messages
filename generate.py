71d082125c6c73a82cf7851a03c70fcc07d2090d
Natan Vilchis <contacto@natanvilchis.org>
2025-06-16 09:52:52 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,27 +1,18 @@
-import numpy
-from cryptography.fernet import Fernet
-import base64
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import hashlib 
+from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
+import base64 
 
-
-key = "key"
 kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt="salt".encode(),
-    iterations=1,
+    algorithm=hashes.SHA512(),
+    length=13,
+    salt=hashlib.sha512("HELLO".encode()).digest(),
+    iterations=13,
     backend=default_backend()
 )
-key = base64.urlsafe_b64encode(kdf.derive(key.encode()))
+key = hashlib.sha512("WORLD".encode()).digest()
+key = base64.urlsafe_b64encode(key)
+print(key)
 f = Fernet(key)
-
-message = """
-TOP SECRET
-"""
-encrypted = f.encrypt(message.encode())
-print(encrypted.decode())
-
-decrypted = f.decrypt(encrypted)
-print(decrypted.decode())
